b98867be35a2ee014fac0effac0ae3374fb65fa6
marcushugo <phantomboss300@yahoo.com>
2023-02-01 16:51:36 -0600

    added a dark/light theme w/a toggle

--- .eleventy.js
+++ .eleventy.js
@@ -3,10 +3,11 @@ const { DateTime } = require('luxon')
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addWatchTarget('./src/sass');
-  eleventyConfig.addPassthroughCopy('./src/css');
+  // eleventyConfig.addPassthroughCopy('./src/css');
   eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addPassthroughCopy('./src/js/app.js');
   eleventyConfig.addPassthroughCopy('./src/admin');
-    
+ 
   eleventyConfig.addFilter('postDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
   })
