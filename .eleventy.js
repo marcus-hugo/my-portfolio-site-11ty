357a22d0aa9a5ef2e454af901c59d4e9f68ee724
marcushugo <phantomboss300@yahoo.com>
2023-02-06 09:28:50 -0600

    many improvements

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,20 @@
-const { DateTime } = require('luxon')
+const { DateTime } = require("luxon")
 
-module.exports = function(eleventyConfig) {
-
-  eleventyConfig.addWatchTarget('./src/sass');
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./src/sass")
   // eleventyConfig.addPassthroughCopy('./src/css');
-  eleventyConfig.addPassthroughCopy('./src/assets');
-  eleventyConfig.addPassthroughCopy('./src/js/app.js');
-  eleventyConfig.addPassthroughCopy('./src/admin');
- 
-  eleventyConfig.addFilter('postDate', (dateObj) => {
+  eleventyConfig.addPassthroughCopy("./src/assets")
+  eleventyConfig.addPassthroughCopy("./src/js/")
+  eleventyConfig.addPassthroughCopy("./src/admin")
+
+  eleventyConfig.addFilter("postDate", dateObj => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
   })
 
   return {
     dir: {
-      input: 'src',
-      output: 'public'
+      input: "src",
+      output: "public"
     }
   }
 }
