af5f0de9b0734d1b4f88a0f0e1c22767bf44e882
marcushugo <phantomboss300@yahoo.com>
2023-04-06 09:59:57 -0500

    added prisim js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const { DateTime } = require("luxon")
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass")
@@ -6,6 +7,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets")
   eleventyConfig.addPassthroughCopy("./src/js/")
   eleventyConfig.addPassthroughCopy("./src/admin")
+  eleventyConfig.addPlugin(syntaxHighlight)
 
   eleventyConfig.addFilter("postDate", dateObj => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
